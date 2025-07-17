289fed38052aeccc6bcbfd6d8359f8311a2b8e5f
doneni <xxerrenn@gmail.com>
2025-07-17 17:58:27 +0900

    feat : Add link to posts at home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,13 @@
 import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
-  return <h1>🍰 Real Qwik Blog</h1>;
+  return (
+    <>
+      <h1>🍰 Real Qwik Blog</h1>
+      <a>🍓</a>
+      <a href="/posts" style={{ display: 'inline-block', marginTop: '1rem', fontWeight: 'bold' }}>
+        Posts
+      </a>
+    </>
+  );
 });

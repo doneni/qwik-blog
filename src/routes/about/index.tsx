7857f3f7c1685da94dd9c4d25a36afed7b704e91
doneni <xxerrenn@gmail.com>
2025-07-17 18:11:34 +0900

    feat : Update about index.tsx

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -2,11 +2,11 @@ import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <section>
-      <h1>About</h1>
+    <>
+      <h1>🍓 About</h1>
       <p>
         I'm Doneni.
       </p>
-    </section>
+    </>
   );
 });

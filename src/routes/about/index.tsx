771d070055df3e753ac087d202af138f42bc8a21
doneni <xxerrenn@gmail.com>
2025-07-17 20:21:34 +0900

    feat : Update about index.tsx

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -1,12 +1,13 @@
 import { component$ } from '@builder.io/qwik';
+import thumpsUpGuu from '../../assets/thumbs-up-guu.jpeg';
 
 export default component$(() => {
   return (
     <>
       <h1>🍓 About</h1>
-      <p>
-        I'm Doneni.
-      </p>
+      <p>I'm Doneni.</p>
+      <br />
+      <img src={thumpsUpGuu} alt="Thumbs Up Guu" width="300" />
     </>
   );
 });

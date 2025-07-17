662059e08937a05b99c19284790fa101509e14df
doneni <xxerrenn@gmail.com>
2025-07-17 17:19:15 +0900

    feat : Add header and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer class="w-full border-t py-4 px-4 text-sm text-gray-500 text-center">
+      © {new Date().getFullYear()} Doneni. All rights reserved.
+    </footer>
+  );
+}

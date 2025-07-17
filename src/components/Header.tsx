d93437e2ad77da6d12dc8c7a3e2a598659f004e2
doneni <xxerrenn@gmail.com>
2025-07-17 18:31:12 +0900

    feat : Update header home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header class="w-full border-b py-4 px-4 text-xl font-bold">
-      <a href="/">Doneni's Blog</a>
+      <a href="/">Home</a>
     </header>
   );
 }

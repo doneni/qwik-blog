9cfdccaafb802af620e93e866697f56af47c54f5
doneni <xxerrenn@gmail.com>
2025-07-17 20:19:26 +0900

    feat : Update home index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import { component$ } from '@builder.io/qwik';
+import nappingCat from '../assets/napping-cat.jpeg';
 
 export default component$(() => {
   return (
     <>
       <p> Hi there! This is a simple blog built with Qwik. </p>
       <p> You can document any thoughts, experiments, and lessons learned.</p>
+      <br/>
+      <img src={nappingCat} alt="Napping Cat" width="400" />
     </>
   );
 });

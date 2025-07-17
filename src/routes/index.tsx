81e2114f5a7e2b9e116de9d9170013e297663c68
doneni <xxerrenn@gmail.com>
2025-07-17 18:04:15 +0900

    feat : Add simple introduction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,19 @@ import { component$ } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <>
-      <h1>🍰 Real Qwik Blog</h1>
-      <a>🍓</a>
-      <a href="/posts" style={{ display: 'inline-block', marginTop: '1rem', fontWeight: 'bold' }}>
-        Posts
-      </a>
+      <section>
+        <h1>🍰 Real Qwik Blog</h1>
+        <p>
+          Hi there! This is a simple blog built with Qwik.
+        </p>
+      </section>
+
+      <section>
+        <a>🍓</a>
+        <a href="/posts" style={{ display: 'inline-block', marginTop: '1rem', fontWeight: 'bold' }}>
+          Posts
+        </a>
+      </section>
     </>
   );
 });
